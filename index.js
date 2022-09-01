239675b7ac2157bc0e7faa09c5c156b609f33810
hogan <hogan.r.sherrow@vanderbilt.edu>
2022-09-01 10:11:25 -0500

    writeFile successfully generates index.html in dist. copyFile copies over stylesheet.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-
+const pageTemplate = require('./src/page-template.js');
+const { writeFile, copyFile } = require('./src/page-generator.js');
 
 const promptUser = () => {
     return inquirer.prompt([{
@@ -8,3 +9,7 @@ const promptUser = () => {
         name: ''
     }])
 };
+
+copyFile();
+writeFile(pageTemplate());
+console.log("I made it to the end.");
